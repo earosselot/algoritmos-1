ce2a8d888217776ed3fd44b291df06d13baf70fa
earosselot <earosselot@gmail.com>
2021-10-18 10:50:55 -0300

    avances labo 6

--- Labo/Labo6/template-alumnos/ej1/Test/puntajeTEST.cpp
+++ Labo/Labo6/template-alumnos/ej1/Test/puntajeTEST.cpp
@@ -2,3 +2,38 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+TEST(puntajeTest, menor10Divisible3)  {
+  //setup
+  int b = 3;
+  // exercise
+  int result = puntaje(b);
+  // check
+  EXPECT_EQ(result, 16);
+}
+
+TEST(puntajeTest, menor10NoDivisible3) {
+  //setup
+  int b = 1;
+  // exercise
+  int result = puntaje(b);
+  // check
+  EXPECT_EQ(result, -8);
+}
+
+TEST(puntajeTest, mayor10Divisible3) {
+  //setup
+  int b = 12;
+  // exercise
+  int result = puntaje(b);
+  // check
+  EXPECT_EQ(result, 22);
+}
+
+TEST(puntajeTest, mayor10NoDivisible3) {
+  //setup
+  int b = 16;
+  // exercise
+  int result = puntaje(b);
+  // check
+  EXPECT_EQ(result, 6);
+}
--- Labo/Labo6/template-alumnos/ej2/Test/llenarTaxisTEST.cpp
+++ Labo/Labo6/template-alumnos/ej2/Test/llenarTaxisTEST.cpp
@@ -1,5 +1,124 @@
 #include "../llenarTaxis.h"
 #include "gtest/gtest.h"
 
-// Escribir tests aca:
+// llenarTaxis 1
 
+TEST(llenarTaxis1, todasMismoValor) {
+  int a = 4;
+  int b = 4;
+  int c = 4;
+
+  int result = llenarTaxis1(a, b, c);
+
+  EXPECT_EQ(result, 6);
+}
+
+TEST(llenarTaxis1, solosJuntos) {
+  int a = 6; // 1 (1p+1p)
+  int b = 4; // 2 (2p+2p)
+  int c = 4; // 4 (3p+1p)
+
+  int result = llenarTaxis1(a, b, c);
+
+  EXPECT_EQ(result, 7);
+}
+
+TEST(llenarTaxis1, masSolosJuntos) {
+  int a = 2; // -
+  int b = 6; // 3 (2p+2p)
+  int c = 5; // 2 (3p+1p) 3(3p)
+
+  int result = llenarTaxis1(a, b, c);
+
+  EXPECT_EQ(result, 8);
+}
+
+// TEST(llenarTaxis1, nombre) {
+//   int a = numero;
+//   int b = numero;
+//   int c = numero;
+
+//   int result = llenarTaxis1(a, b, c);
+
+//   EXPECT_EQ(result, reslutadoEsperado);
+// }
+
+//--------------------------------------
+//--------------------------------------
+//--------------------------------------
+// llenarTaxis 2
+
+TEST(llenarTaxis2, todasMismoValor2) {
+  int a = 4;
+  int b = 4;
+  int c = 4;
+
+  int result = llenarTaxis2(a, b, c);
+
+  EXPECT_EQ(result, 6);
+}
+
+TEST(llenarTaxis2, solosJuntos2) {
+  int a = 6;
+  int b = 4;
+  int c = 4;
+
+  int result = llenarTaxis2(a, b, c);
+
+  EXPECT_EQ(result, 7);
+}
+
+TEST(llenarTaxis2, masSolosJuntos2) {
+  int a = 2;
+  int b = 6;
+  int c = 5;
+
+  int result = llenarTaxis2(a, b, c);
+
+  EXPECT_EQ(result, 8);
+}
+
+// TEST(llenarTaxis2, nombre) {
+//   int a = numero;
+//   int b = numero;
+//   int c = numero;
+
+//   int result = llenarTaxis2(a, b, c);
+
+//   EXPECT_EQ(result, reslutadoEsperado);
+// }
+
+//--------------------------------------
+//--------------------------------------
+//--------------------------------------
+// llenarTaxis 3
+
+TEST(llenarTaxis3, todasMismoValor2) {
+  int a = 4;
+  int b = 4;
+  int c = 4;
+
+  int result = llenarTaxis3(a, b, c);
+
+  EXPECT_EQ(result, 6);
+}
+
+TEST(llenarTaxis3, solosJuntos3) {
+  int a = 6;
+  int b = 4;
+  int c = 4;
+
+  int result = llenarTaxis3(a, b, c);
+
+  EXPECT_EQ(result, 7);
+}
+
+TEST(llenarTaxis3, masSolosJuntos3) {
+  int a = 2;
+  int b = 6;
+  int c = 5;
+
+  int result = llenarTaxis3(a, b, c);
+
+  EXPECT_EQ(result, 8);
+}
